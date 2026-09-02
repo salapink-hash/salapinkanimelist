0b8dc7b208dda3b66352bb9b0d1a4f8ba58e13d3
CuyAnimeList Student <cuyanimelist@example.com>
2026-09-02 17:41:49 +0700

    feat: add google authentication

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,16 @@
 import NextAuth from "next-auth"
 import githubAuth from "next-auth/providers/github"
+import googleAuth from "next-auth/providers/google"
 
 export const authOption = {
   providers: [
     githubAuth({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    googleAuth({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     })
   ],
   secret: process.env.NEXTAUTH_SECRET
