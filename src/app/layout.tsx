5b0d1cb120cc8feac16e601c5c9fe798558ad5ab
CuyAnimeList Student <cuyanimelist@example.com>
2026-09-03 14:49:59 +0700

    feat: integrate adsterra ad network scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,10 +17,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google AdSense */}
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4671153148947663"
           crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+        {/* Adsterra Network Unit 1 */}
+        <Script
+          src="https://pl31162658.profitableratecpmnetwork.com/04/da/50/04da50adaeb210f2d909ee5c21a53924.js"
+          strategy="afterInteractive"
+        />
+        {/* Adsterra Network Unit 2 */}
+        <Script
+          src="https://pl31162659.profitableratecpmnetwork.com/2b/f9/08/2bf908f02f4dfe5fc6eebcc4d08adc9a.js"
           strategy="afterInteractive"
         />
       </head>
