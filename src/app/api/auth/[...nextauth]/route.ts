d1c8aad3a9f7af92109e60a1ca8576545ba0da83
CuyAnimeList Student <cuyanimelist@example.com>
2026-09-03 11:36:04 +0700

    fix: allow account linking for google and github auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -7,10 +7,12 @@ export const authOption = {
     githubAuth({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
+      allowDangerousEmailAccountLinking: true,
     }),
     googleAuth({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      allowDangerousEmailAccountLinking: true,
     })
   ],
   secret: process.env.NEXTAUTH_SECRET
