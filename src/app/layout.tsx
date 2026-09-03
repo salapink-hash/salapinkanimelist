767fb2bee6151d059d1456cda2be3f7f6815866f
CuyAnimeList Student <cuyanimelist@example.com>
2026-09-03 14:57:50 +0700

    chore: remove adsterra scripts to maintain clean site for adsense review

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,16 +24,6 @@ export default function RootLayout({
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
-        {/* Adsterra Network Unit 1 */}
-        <Script
-          src="https://pl31162658.profitableratecpmnetwork.com/04/da/50/04da50adaeb210f2d909ee5c21a53924.js"
-          strategy="afterInteractive"
-        />
-        {/* Adsterra Network Unit 2 */}
-        <Script
-          src="https://pl31162659.profitableratecpmnetwork.com/2b/f9/08/2bf908f02f4dfe5fc6eebcc4d08adc9a.js"
-          strategy="afterInteractive"
-        />
       </head>
       <body>
         <Navbar />
