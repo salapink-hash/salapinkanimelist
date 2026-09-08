4100e9642e534c984f6aaa08bf76b10403fcb7ed
CuyAnimeList Student <cuyanimelist@example.com>
2026-09-09 04:43:51 +0700

    feat: add Monetag Vignette banner tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,6 +76,14 @@ export default function RootLayout({
             __html: `(function(s){s.dataset.zone='11756229',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`
           }}
         />
+        {/* Monetag Vignette Banner */}
+        <Script
+          id="monetag-vignette"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(s){s.dataset.zone='11756231',s.src='https://n6wxm.com/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`
+          }}
+        />
       </head>
       <body suppressHydrationWarning>
         <Navbar />
